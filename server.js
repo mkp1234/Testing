9f555c010f33b82597723d9874c75bc155ca3523
Manish Kumar <kumarmanish12762@gmail.com>
2026-04-21 23:02:00 +0530

    New changes

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ app.get("/", (req, res) => {
     res.status(200).json("Server running.")
 })
 
-const port = 3000;
+// const port = 3000;
+const port = process.env.PORT || 3000;
 
 connectDB().then(()=>{
     app.listen(port, () => {
